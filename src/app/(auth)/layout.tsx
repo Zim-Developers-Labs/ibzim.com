81cdb98ce90c7283406e4136f262cd522e7c51a9
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2025-05-25 16:32:18 +0200

    auth progress check

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,36 @@
+import '../globals.css';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'IBZim Auth',
+  description: 'Create or Login to your IB Account to access IBZim',
+};
+
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${inter.className} h-full antialiased`}
+    >
+      <body>
+        <Toaster />
+        {children}
+        <Analytics />
+        <SpeedInsights />
+        <GoogleAnalytics gaId={process.env.GA_SECRET!} />
+      </body>
     </html>
   );
 }
