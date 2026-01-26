9651534490cd633875de02b1bec98375db34c604
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-01-26 13:42:31 +0200

    seo update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,13 @@
 import { Metadata } from 'next';
 import HomeComponent from './component';
 import { preparePageMetadata } from '@/lib/metadata';
-import { siteConfig } from '@/lib/config';
 import { getSearchData } from '@/lib/sanity/actions';
 
 export const generateMetadata = (): Metadata =>
   preparePageMetadata({
-    title: 'IBZIM: Zimbabwe First Search Engine',
+    title: 'IBZIM: Search & information platform for Zimbabwe',
     description:
-      'IBZIM is a platform for sharing and discovering information about Zimbabwe through various tools such as tables, biographies, articles and more.',
+      'IBZIM is a search engine and information platform for knowledge about Zimbabwe through various tools such as tables, articles and more.',
     pageUrl: '/',
     imageUrl: '/banner.webp',
   });
