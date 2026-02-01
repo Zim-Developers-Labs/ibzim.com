4cf9bbad5a54ce40e5e5ee2ddac2c1569e82a0e5
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-02-01 21:36:28 +0200

    feat: update page title for improved clarity and branding

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { preparePageMetadata } from '@/lib/metadata';
 
 export const generateMetadata = (): Metadata =>
   preparePageMetadata({
-    title: 'IBZIM: Search & information platform for Zimbabwe',
+    title: 'IBZIM: Search Engine & Information Hub',
     description:
       'IBZIM is a search engine and information platform for knowledge about Zimbabwe through various tools such as tables, articles and more.',
     pageUrl: '/',
